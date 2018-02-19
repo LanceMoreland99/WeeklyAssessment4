855e829adbcc417557e67540c930262168cf65b1
Lance Moreland <lmoreland1899@gmail.com>
2018-02-19 16:00:29 -0500

    play a game

--- src/com/company/Question_4/Main.java
+++ src/com/company/Question_4/Main.java
@@ -1,6 +1,10 @@
 package com.company.Question_4;
 
+import java.util.Scanner;
+
 public class Main {
+
+    private Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
         /*
@@ -10,6 +14,23 @@ public class Main {
         teleported to the beach".  If the number is between 91 and/or equal to 100 display, "Well, I hope you like it hot,
         you have been teleported to a volcano."
          */
+        Main main = new Main();
+        main.playAGame();
     }
 
+    int userInput = input.nextInt();
+    private void playAGame (){
+
+        if(userInput <=45){
+            System.out.println("You have been teleported to a swamp");
+        }
+
+        else if(userInput <= 90){
+            System.out.println("Lucky you, you've been teleported to the beach");
+        }
+
+        else if(userInput <= 100){
+            System.out.println("Well, I hope you like it hot, you have been teleported to a volcano");
+        }
+    }
 }
